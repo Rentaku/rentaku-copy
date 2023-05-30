ee760dbc009711501e2563482b268549beab0611
tata <tattadv.1997@gmail.com>
2023-05-30 22:22:04 +0700

    rekomendasi

--- app/src/main/java/com/example/rentakucapstone/dashboard/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/rentakucapstone/dashboard/ui/home/HomeFragment.kt
@@ -4,15 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.rentakucapstone.databinding.FragmentHomeBinding
+
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
+    private lateinit var adapter : RekomendasiAdapter
+    private val names = listOf("ZX 250R", "ZX 250R", "ZX 250R", "ZX 250R")
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -32,9 +36,9 @@ class HomeFragment : Fragment() {
         val root: View = binding.root
 
 
-        val textView: TextView = binding.textHome
+        //val textView: TextView = binding.textHome
         homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+            //textView.text = it
         }
         return root
 
@@ -42,8 +46,23 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        adapter = RekomendasiAdapter(names)
+
+        var recyclerView: RecyclerView
+        binding.rvRekomendasi.layoutManager = LinearLayoutManager(requireActivity())
+        binding.rvRekomendasi.setHasFixedSize(true)
+        binding.rvRekomendasi.adapter = adapter
+
+        val horizontalLayout = LinearLayoutManager(
+            requireActivity(),
+            LinearLayoutManager.HORIZONTAL,
+            false
+        )
+        binding.rvRekomendasi.layoutManager = horizontalLayout
+        binding.rvRekomendasi.layoutManager
+
+        val homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
 
         with(binding) {
             searchView.setupWithSearchBar(searchBar)
---
+++ app/src/main/java/com/example/rentakucapstone/dashboard/ui/home/RekomendasiAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.rentakucapstone.dashboard.ui.home
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.rentakucapstone.R
+
+class RekomendasiAdapter(
+    private val list: List<String>
+) :
+    RecyclerView.Adapter<RekomendasiAdapter.MyViewHolder>() {
+
+    class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val tvRekomendasi: TextView = view.findViewById(R.id.tvRekomendasi)
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): MyViewHolder {
+
+        val itemView: View = LayoutInflater
+            .from(parent.context)
+            .inflate(
+                R.layout.rekomendasi_item,
+                parent,
+                false
+            )
+        return MyViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.tvRekomendasi.text = list[position]
+    }
+
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+}
