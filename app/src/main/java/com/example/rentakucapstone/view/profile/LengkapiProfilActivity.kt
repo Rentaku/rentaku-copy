0de7e693073fe13ede90f9d4f59473f6a4f9be2b
mutiadhewanty <mutiadhewanty@gmail.com>
2023-06-02 13:20:22 +0700

    renter

--- app/src/main/java/com/example/rentakucapstone/view/profile/LengkapiProfilActivity.kt
+++ app/src/main/java/com/example/rentakucapstone/view/profile/LengkapiProfilActivity.kt
@@ -1,12 +1,26 @@
 package com.example.rentakucapstone.view.profile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.example.rentakucapstone.R
+import com.example.rentakucapstone.databinding.ActivityLengkapiProfilBinding
 
 class LengkapiProfilActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLengkapiProfilBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_lengkapi_profil)
+        binding = ActivityLengkapiProfilBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun onNextClick(view: View) {
+        binding.nextTextView.setOnClickListener {
+            startActivity(Intent(this, LengkapiProfilActivity2::class.java))
+            Toast.makeText(this, "Data berhasil Disimpan", Toast.LENGTH_SHORT).show()
+        }
     }
 }
