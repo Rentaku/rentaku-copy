994e0aea91168e82665fc2265c698d73d4bcdf71
mutiadhewanty <mutiadhewanty@gmail.com>
2023-06-16 02:47:42 +0700

    model done

--- app/src/main/java/com/example/rentakucapstone/view/profile/Pembayaran1Activity.kt
+++ app/src/main/java/com/example/rentakucapstone/view/profile/Pembayaran1Activity.kt
@@ -3,11 +3,17 @@ package com.example.rentakucapstone.view.profile
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.rentakucapstone.R
+import com.example.rentakucapstone.databinding.ActivityDetailBinding
+import com.example.rentakucapstone.databinding.ActivityDetailPesananBinding
+import com.example.rentakucapstone.databinding.ActivityPembayaran1Binding
 
 class Pembayaran1Activity : AppCompatActivity() {
+    private lateinit var binding : ActivityPembayaran1Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pembayaran1)
+
+        binding = ActivityPembayaran1Binding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         supportActionBar?.hide()
     }
